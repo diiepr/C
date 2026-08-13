5b2a4a3c2c125e7cc02efa23460134393429f36d
Diego <digopires43@gmail.com>
2026-08-13 14:37:35 -0300

    Atualiza lógica do exercício

--- L1Ex3.c
+++ L1Ex3.c
@@ -14,6 +14,7 @@ int main()
         printf("\n\nDigite um numero para a segunda matriz: ");
         scanf("%i", &Matriz2[Cont]);
     }
+    printf("\nA intercalacao das duas matrizes e: \n");
     for (Cont = 0; Cont <=49; Cont++)
     {
         Matriz3[Cont * 2] = Matriz1[Cont];
